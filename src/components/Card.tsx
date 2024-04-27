39237b780f5bcbe7423ce5dc9d4286cab0ab776e
Sanooj Es <sanoojes6371@gmail.com>
2024-04-28 03:05:43 +0530

    styles: add sm config

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 const Card = ({ mainTxt, desc, pathToImg, imgAlt }: CardProps) => {
     return (
-        <div className="flex justify-around items-center w-full gap-2 bg-neutral-50 bg-opacity-40 px-2 py-4 border-4 border-neutral-50 border-opacity-10 shadow-sm rounded-2xl">
+        <div className="flex max-sm:flex-col max-sm:text-center justify-around items-center w-full gap-2 bg-neutral-50 bg-opacity-40 px-2 py-4 border-4 border-neutral-50 border-opacity-10 shadow-sm rounded-2xl">
             <img className="brightness-0" src={pathToImg} alt={imgAlt} />
             <div className="flex flex-col">
                 <span className="text-3xl text-neutral-950 font-bold">
